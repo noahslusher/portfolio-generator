78c9ad3ac15f66d037b8bec77eabeb16d36f25ed
Carl Wheezer <slushern42@gmail.com>
2022-03-07 11:48:57 -0700

    created dynamic HTML file with node and modularized functions

---
+++ app.js
@@ -0,0 +1,12 @@
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs
+const fs = require('fs')
+const generatePage = require('./src/page-template')
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+ if (err) throw new Error(err);
+
+ console.log('Portfolio complete! Check out index.html to see the output')
+})
